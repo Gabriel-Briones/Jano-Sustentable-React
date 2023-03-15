b95279d9ecc054664727ae2de3abbcb3eee34e80
Gabriel Briones <gamiz08@hotmail.com>
2023-03-15 15:21:06 -0300

    verificación correos

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -14,65 +14,78 @@ export const Checkout = () => {
         const cliente = Object.fromEntries(data)
         const aux = [...carrito]
 
-        aux.forEach(prodCarrito => { //Descontar stock de BDD
-            getProducto(prodCarrito.id).then(prodBDD => {
-                prodBDD.stock -= prodCarrito.cant //Descontar stock 
-                updateProducto(prodBDD.id, prodBDD)
-            })
-        })
+        console.log(cliente)
 
-        createOrdenCompra(cliente, aux, totalPrice(), new Date().toISOString()).then(ordenCompra => {
-            toast(`Gracias por comprar con nosotros!, su orden de compra con el id ${ordenCompra.id} por un total de $ ${new Intl.NumberFormat('de-DE').format(totalPrice())} fue realizada con éxito`, {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
+        if (cliente.email === cliente.email2) {
+
+            aux.forEach(prodCarrito => { //Descontar stock de BDD
+                getProducto(prodCarrito.id).then(prodBDD => {
+                    prodBDD.stock -= prodCarrito.cant //Descontar stock 
+                    updateProducto(prodBDD.id, prodBDD)
+                })
+            })
+
+            createOrdenCompra(cliente, aux, totalPrice(), new Date().toISOString()).then(ordenCompra => {
+                toast(`Gracias por la compra!, su orden de compra es ${ordenCompra.id} por un total de $ ${new Intl.NumberFormat('de-DE').format(totalPrice())} fue realizada con éxito`, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                });
+                e.target.reset()
+                emptyCart()
+                navigate("/")
+            })
+        } else {
+            toast.error(`Los correos electrónicos deben ser iguales, por favor verificar`, {
+                position: toast.POSITION.TOP_RIGHT
             });
-            e.target.reset()
-            emptyCart()
-            navigate("/")
-        })
+        }
     }
 
-    return (
-        <>
-            {carrito.length === 0
-                ?
-                <>
-                    <h2>Para finalizar la compra debe cargar productos en el carrito</h2>
-                    <Link className="nav-link" to={"/"}><button className="btn btn-primary">Continuar comprando</button></Link>
-                </>
-                :
-                <div className="container contForm">
-                    <form onSubmit={consultarForm} ref={datosForm}>
-                        <div className="mb-3">
-                            <label htmlFor="nombre" className="form-label">Nombre y Apellido</label>
-                            <input type="text" className="form-control" name="nombre" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Email</label>
-                            <input type="email" className="form-control" name="email" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="dni" className="form-label">Documento</label>
-                            <input type="number" className="form-control" name="dni" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="celular" className="form-label">Numero telefonico</label>
-                            <input type="number" className="form-control" name="celular" />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="direccion" className="form-label">Direccion</label>
-                            <input type="text" className="form-control" name="direccion" />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Finalizar compra</button>
-                    </form>
-                </div>
-            }
-        </>
-    )
-}
+        return (
+            <>
+                {carrito.length === 0
+                    ?
+                    <>
+                        <h2>Para finalizar la compra debe cargar productos en el carrito</h2>
+                        <Link className="nav-link" to={"/"}><button className="btn btn-primary">Continuar comprando</button></Link>
+                    </>
+                    :
+                    <div className="container contForm">
+                        <form onSubmit={consultarForm} ref={datosForm}>
+                            <div className="mb-3">
+                                <label htmlFor="nombre" className="form-label">Nombre y Apellido</label>
+                                <input type="text" className="form-control" name="nombre" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input type="email" className="form-control" name="email" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email2" className="form-label">Repetir el Email</label>
+                                <input type="email" className="form-control" name="email2" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="dni" className="form-label">Documento</label>
+                                <input type="number" className="form-control" name="dni" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="celular" className="form-label">Numero telefonico</label>
+                                <input type="number" className="form-control" name="celular" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="direccion" className="form-label">Direccion</label>
+                                <input type="text" className="form-control" name="direccion" />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Finalizar compra</button>
+                        </form>
+                    </div>
+                }
+            </>
+        )
+    }
